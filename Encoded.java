463e857c75abb14b59c65e076f2a86e40b610d6d
ReinaDNA <xxzhijiexx@gmail.com>
2026-05-03 16:36:49 +0800

    Created Encoded.java
    Initial class skeleton commit

---
+++ Encoded.java
@@ -0,0 +1,35 @@
+public class Encoded {
+    String inputText;
+    int charCount;
+    String resultText;
+    String GroupID;
+
+    //Constructor
+    public Encoded(){
+
+    }
+
+    public Encoded(String inputText){
+        this.inputText = inputText;
+    }
+
+    public int countCharacters(String inputText){
+        return 0;
+    }
+
+    public boolean checkStringValidity(String inputText){
+        return false;
+    }
+
+    public int generateShift(){
+        return 0;
+    }
+
+    public String applyCipher(String inputText, int shift){
+        return "";
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
