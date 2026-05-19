9f25abc13ff48425f8ceb08d5cbbbf06318ae69e
Thiveyabaskaran <122294169+Thiveyabaskaran@users.noreply.github.com>
2026-05-19 13:33:34 +0800

    Add Counting Method

--- Encoded.java
+++ Encoded.java
@@ -27,6 +27,15 @@ public class Encoded {
         int finalShift = groupShift + this.charCount;
         return finalShift;
     }
+      public int countCharacters(String inputText) {
+        int count = 0;
+        for (int i = 0; i < inputText.length(); i++) {
+            if (inputText.charAt(i) != ' ') {
+                count++;
+            }
+        }
+        return count;
+    }
 
     public String applyCipher(String inputText, int shift){
         String encryptedText = "";
