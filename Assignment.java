1511b1a165609051128291661914dec9ac45e7c4
ReinaDNA <xxzhijiexx@gmail.com>
2026-05-24 06:59:05 +0800

    More comments
    I forgot about assignment.java

--- Assignment.java
+++ Assignment.java
@@ -1,6 +1,9 @@
 public class Assignment {
     public static void main(String[] args) {
-        Encoded myWindow = new Encoded();
+        Encoded myWindow = new Encoded(); // Invoke the program
         myWindow.setVisible(true);
     }
 }
+
+// Main file execution
+// Contributed by Arif Amirul Aiman bin Marzuki (83282)
