fd0a7835690ee40aec44f34f09af313c091cb103
ReinaDNA <xxzhijiexx@gmail.com>
2026-05-24 06:43:47 +0800

    Comments
    Added contribution and explanation comments

--- Encoded.java
+++ Encoded.java
@@ -30,7 +30,7 @@ public class Encoded extends JFrame {
         displayArea.setEditable(false); // So the user can't type in the result box
         
         // 3. Add them to the window
-        add(new JLabel("Enter text (lowercase & numbers):"));
+        add(new JLabel("Enter text (lowercase, numbers & spaces only):"));
         add(inputField);
         add(encodeButton);
         add(new JScrollPane(displayArea));
@@ -47,6 +47,7 @@ public class Encoded extends JFrame {
                 if (!checkStringValidity(inputText)) {
                     // Show a simple pop-up error
                     JOptionPane.showMessageDialog(null, "Invalid input! Use lowercase letters, digits, and spaces only.");
+                    
                 } else {
                     // If it is valid, use Thiveya and Zhi Jie's methods
                     int count = countCharacters(inputText);
@@ -65,10 +66,13 @@ public class Encoded extends JFrame {
     }
 
     // Constructor with predefined input string
+    //Contributor: Arif Amirul Aiman Bin Marzuki (83282)
     public Encoded(String inputText){
         this.inputText = inputText;
     }
 
+    // Method for counting non-space characters
+    // Contributor: Thiveya Shree a/p Baskaran (106564)
      public int countCharacters(String inputText) {
         // Resets the variable to original state
         charCount = 0;
@@ -80,31 +84,49 @@ public class Encoded extends JFrame {
         return charCount;
     }
 
+    // Method for input validation
+    // Contributor: Mohamad Ikhmal Iskandar bin Mohd Ibrahim  (105016)
     public boolean checkStringValidity(String inputText){
-        // Maliska's part
+        boolean onlyValidAlphanumeric = inputText.matches("[a-z0-9\s]+"); // See if it matches any of the a to z, 0 to 9 and whitespace
+        // If the text field is not an empty field
+        if (!inputText.isEmpty()) {
+            if (onlyValidAlphanumeric) {
+                return true;
+            } 
+        // If the text field is empty or null
+        } else if ((inputText == null) || inputText.isEmpty()) {
+           return false;
+        } 
         return false;
     }
 
+    // Method for generating shift for encoding
+    // Contributor: Seng Zhi Jie (106256)
     public int generateShift(){
-        int groupShift = Math.abs((this.GroupID.hashCode() % 10)) + 1;
-        int finalShift = groupShift + charCount;
+        int groupShift = Math.abs((this.GroupID.hashCode() % 10)) + 1; // Generate hash between 1 to 10
+        int finalShift = groupShift + charCount; // Calculate final shift
         return finalShift;
     }
    
+    // Method for encoding input text
+    // Contributor: Seng Zhi Jie (106256)
     public String applyCipher(String inputText, int shift){
-
+        // Reset the result text
+        resultText = "";
         for(int i=0; i<inputText.length(); i++){
-            
+            // Iterating through the string to apply encryption
             char c = inputText.charAt(i);
             
+            // Letters encryption
             if(Character.isLowerCase(c)){
                 char base = 'a';
-                int shiftedPosition = (c - base + shift) % 26 + base;
-                resultText += (char)(shiftedPosition);
+                int shiftedPosition = (c - base + shift) % 26 + base; // Encryption formula for letters
+                resultText += (char)(shiftedPosition); // Revert back to chart and add it into result
             }else if(Character.isDigit(c)){
+                // Numbers encryption
                 char base = '0';
-                int shiftedPosition = (c - base + shift) % 10 + base;
-                resultText += (char)(shiftedPosition);
+                int shiftedPosition = (c - base + shift) % 10 + base; // Encryption formula for numbers
+                resultText += (char)(shiftedPosition); // Revert back to char and add it into result 
             }else{
                 // The char is a white space, no encryption needed
                 resultText += c;
